a48c78d1ab8f67b500c23e6c7ee454966ddab354
Guillaume Sere <guillaumesere@hotmail.fr>
2026-03-10 11:55:34 +0100

    add sitemap and validation google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,68 @@
+import type { Metadata } from "next"
 import "./globals.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+
+export const metadata: Metadata = {
+  title: "Calcul Ovulation & Suivi du Cycle | Fertilité & Grossesse",
+  description:
+    "Application gratuite pour suivre son cycle menstruel, calculer l'ovulation, connaître la période fertile et augmenter ses chances de grossesse.",
+  
+  keywords: [
+    "ovulation",
+    "calcul ovulation",
+    "cycle menstruel",
+    "fertilité",
+    "calendrier ovulation",
+    "suivi cycle femme",
+    "période fertile",
+    "grossesse"
+  ],
+
+  robots: "index, follow",
+
+  openGraph: {
+    title: "Suivi du Cycle & Calcul Ovulation",
+    description:
+      "Découvrez vos jours fertiles grâce à un calendrier intelligent et un score de fertilité.",
+    url: "https://ovulation-tracker.vercel.app",
+    siteName: "CycleCare",
+    locale: "fr_FR",
+    type: "website"
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Calculateur Ovulation",
+    description:
+      "Suivez votre cycle et découvrez vos jours fertiles facilement."
+  }
+}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-
   return (
-
     <html lang="fr">
 
-      <body>
+        <head>
+            <meta name="google-site-verification" content="nwVPqsKRGvHVh9v-Qn4QoawQzNbN99Sfg6usOSlUEhg" />
+        </head>
+
+      <body className="flex flex-col min-h-screen">
 
         <Header />
 
-        {children}
+        <main className="grow">
+          {children}
+        </main>
 
         <Footer />
 
       </body>
 
     </html>
-
   )
-
 }
---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://ovulation-tracker.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ovulation-tracker.vercel.app/calendar",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ovulation-tracker.vercel.app/history",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ovulation-tracker.vercel.app/advice",
+      lastModified: new Date(),
+    },
+  ]
+}
